21a87b1e659900c7c696722d10845807f9b22207
SANYAM MITTAL <44235818+sanyam1992000@users.noreply.github.com>
2020-01-01 21:06:24 +0530

    changes integer field to biginteger field

--- accounts/models.py
+++ accounts/models.py
@@ -26,11 +26,11 @@ branch = (
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    roll_no = models.IntegerField(unique=True)
+    roll_no = models.BigIntegerField(unique=True, max_length=99999999999)
     course = models.CharField(choices=courses, max_length=10, default='CE')
     branch = models.CharField(choices=branch, max_length=10)
     icard = models.ImageField(upload_to='icard', blank=True, null=True)
-    phoneno = models.IntegerField(unique=True, default=None)
+    phoneno = models.BigIntegerField(default=None)
 
     def __str__(self):
         return str(self.roll_no) + ' , ' + str(self.user)
