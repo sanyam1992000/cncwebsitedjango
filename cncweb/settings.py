aafc46633d2dc17692843be694cba88089ba88a8
Sanyam <sanyam19092000@gmail.com>
2020-01-26 23:58:51 +0530

    1

--- cncweb/settings.py
+++ cncweb/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'blog',
     'accounts',
     'events',
+    'storages',
+    
 ]
 
 MIDDLEWARE = [
@@ -129,8 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # emailid  =  careerandcounsellingcell.ymca@gmail.com
 # password = Career&Counselling
@@ -142,5 +144,18 @@ EMAIL_HOST_PASSWORD = 'Career&Counselling'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+AWS_ACCESS_KEY_ID = ' AKIAWF6VJARJED5XY63X'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cncymca'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
+
 
 django_heroku.settings(locals())
--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,3 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
