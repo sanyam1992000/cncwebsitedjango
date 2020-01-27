ab11217974982cba18772034e7c2712a9db5986c
Sanyam <sanyam19092000@gmail.com>
2020-01-27 22:40:47 +0530

    <pre>

--- cncweb/urls.py
+++ cncweb/urls.py
@@ -36,6 +36,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
 ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
